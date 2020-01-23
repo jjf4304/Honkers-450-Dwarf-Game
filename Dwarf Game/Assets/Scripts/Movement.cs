07ea66cff9233f81b826bd0b11d8b431a8ea8941
mtp8375 <mtp8375@IGM2025-13>
2020-01-23 15:44:05 -0500

    Some improvements on player rotation, still not perfect

--- Dwarf Game/Assets/Scripts/Movement.cs
+++ Dwarf Game/Assets/Scripts/Movement.cs
@@ -13,7 +13,8 @@ public class Movement : MonoBehaviour
     public float sideSpeed;
 
     float rotateShipBy = 0;
-    const float rotationAmount = .1f;
+    const float rotationAmount = 1.0f;
+    const float maxRot = .2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,16 +33,38 @@ public class Movement : MonoBehaviour
         {
             sideSpeed = -5f;
             rotateShipBy = -rotationAmount;
+            if(transform.rotation.z < -maxRot)
+            {
+                rotateShipBy = 0;
+            }
         }
         else if (Input.GetKey(KeyCode.D))
         {
             sideSpeed = 5f;
             rotateShipBy = rotationAmount;
+            if (transform.rotation.z > maxRot)
+            {
+                rotateShipBy = 0;
+            }
         }
-
+        else
+        {
+            if (transform.rotation.z > .01f)
+            {
+                rotateShipBy = -rotationAmount;
+            }
+            else if (transform.rotation.z < -.01f)
+            {
+                rotateShipBy = rotationAmount;
+            }
+            else
+            {
+                rotateShipBy = 0;
+            }
+        }
         transform.Rotate(new Vector3(0f, 0f, rotateShipBy));
+        print(transform.rotation.z);
         rotateShipBy = 0;
-
         rigid.velocity = new Vector2(sideSpeed, downSpeed);
         Vector3 vec = new Vector3(rigid.velocity.x, rigid.velocity.y, 0f);
 
