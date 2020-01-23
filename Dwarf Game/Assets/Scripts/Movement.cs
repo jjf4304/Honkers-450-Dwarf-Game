51de85393d6d6e4cf590e9090b6d352067fc4d24
mtp8375 <mtp8375@IGM2025-13>
2020-01-23 14:48:14 -0500

    movment is mostly working

--- Dwarf Game/Assets/Scripts/Movement.cs
+++ Dwarf Game/Assets/Scripts/Movement.cs
@@ -12,6 +12,8 @@ public class Movement : MonoBehaviour
     public float downSpeed;
     public float sideSpeed;
 
+    float rotateShipBy = 0;
+    const float rotationAmount = .1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,22 +31,30 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             sideSpeed = -5f;
+            rotateShipBy = -rotationAmount;
         }
         else if (Input.GetKey(KeyCode.D))
         {
             sideSpeed = 5f;
+            rotateShipBy = rotationAmount;
         }
 
+        transform.Rotate(new Vector3(0f, 0f, rotateShipBy));
+        rotateShipBy = 0;
+
         rigid.velocity = new Vector2(sideSpeed, downSpeed);
+        Vector3 vec = new Vector3(rigid.velocity.x, rigid.velocity.y, 0f);
 
         
+
+
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag.Equals("Left Wall"))
         {
-            transform.position = new Vector3(collision.transform.position.x + collision.transform.localScale.x / 2, 0f);
-            Debug.Log("AWRAWRAWRAWRAWDFAWF");
+            
         }
 
         if (collision.gameObject.tag.Equals("Right Wall"))
