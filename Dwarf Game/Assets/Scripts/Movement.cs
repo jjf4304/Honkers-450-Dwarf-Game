99c4cade97682b562dd1371267d0d93b506f74a0
Sam Belisle <spb6892@rit.edu>
2020-01-25 14:02:29 -0500

    Add digging trail and Update colors + UI to look better/scale better

--- Dwarf Game/Assets/Scripts/Movement.cs
+++ Dwarf Game/Assets/Scripts/Movement.cs
@@ -11,10 +11,13 @@ public class Movement : MonoBehaviour
     private float offset;
     public float downSpeed;
     public float sideSpeed;
+    public List<GameObject> trailObjects;
 
+    int trailIndex;
     float rotateShipBy = 0;
     const float rotationAmount = 1.0f;
     const float maxRot = .2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +25,35 @@ public class Movement : MonoBehaviour
         sideSpeed = 0f;
         rigid = GetComponent<Rigidbody2D>();
         offset = transform.position.y - cam.transform.position.y;
+        trailIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        HandleMovement();
+        HandleTrail();
+        //Vector3 vec = new Vector3(rigid.velocity.x, rigid.velocity.y, 0f);
+    }
+
+    // Places and moves the trail as the player moves
+    void HandleTrail()
+    {
+        Debug.Log(trailIndex);
+        trailObjects[trailIndex].transform.position = new Vector3(transform.position.x, transform.position.y, 10);
+        trailObjects[trailIndex].transform.rotation = transform.rotation;
+        if (trailIndex == trailObjects.Count - 1)
+        {
+            trailIndex = 0;
+        }
+        else
+        {
+            trailIndex++;
+        }
+    }
+
+    // Takes care of all side to side movement and rotations
+    void HandleMovement()
     {
         cam.transform.position = new Vector3(0, transform.position.y - offset, cam.transform.position.z);
         sideSpeed = 0f;
@@ -33,7 +61,7 @@ public class Movement : MonoBehaviour
         {
             sideSpeed = -5f;
             rotateShipBy = -rotationAmount;
-            if(transform.rotation.z < -maxRot)
+            if (transform.rotation.z < -maxRot)
             {
                 rotateShipBy = 0;
             }
@@ -63,14 +91,7 @@ public class Movement : MonoBehaviour
             }
         }
         transform.Rotate(new Vector3(0f, 0f, rotateShipBy));
-        print(transform.rotation.z);
         rotateShipBy = 0;
         rigid.velocity = new Vector2(sideSpeed, downSpeed);
-        Vector3 vec = new Vector3(rigid.velocity.x, rigid.velocity.y, 0f);
-
-        
-
     }
-
-
 }
