0a0c9e8ba7705d380abe1559b16c6b9b757c7288
Sam Belisle <spb6892@rit.edu>
2020-01-25 16:39:10 -0500

    Add point tracking system

---
+++ Dwarf Game/Assets/Scripts/PointTracker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointTracker : MonoBehaviour
+{
+    private EnergyBar energyScript;
+
+    public GameObject pointDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        energyScript = GetComponent<EnergyBar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointDisplay.GetComponent<Text>().text = energyScript.elapsedTime.ToString();
+    }
+}
